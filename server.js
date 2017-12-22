c134532553044e71a5f08ffe8b033babaee952ff
Welsh, Mark <mark.welsh@accenture.com>
2017-12-22 11:54:01 +0000

    Updated test message

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world test again\n');
+  res.send('Hello world test again and again\n');
 });
 
 app.listen(PORT, HOST);
